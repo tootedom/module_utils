f3b8dc3a7d547e4081120edecd64b1ff6ce82b5c
dominictootell <dominic.tootell@gmail.com>
2015-11-07 15:30:24 +0000

    retry util

---
+++ aws_retry_utils.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+global aws_retry_api_throlling_retries
+aws_retry_api_throlling_retries = 10
+
+class RetryLimitExceeded(Exception):
+    def __init__(self, exception, retries):
+      self.exception = exception
+      self.tries = retries
+
+    def __str__(self):
+      return repr("Throttling retry limit exceeded, no_of_tries(%s), last exception: %s" % (self.tries,self.exception))
+
+
+def get_api_error_code(exception):
+  if hasattr(exception,"body"):
+    if exception.body is not None and hasattr(exception.body,"split"):
+      code = exception.body.split("<Code>")[1]
+      code = code.split("</Code>")[0]
+      return code
+    else:
+      return ""
+  else:
+    return ""
+
+def call_with_retry(api_call,*args,**kwargs):
+  last_exception = ""
+  tries = 0
+  retry_interval = 2
+  retry = aws_retry_api_throlling_retries
+  while True:
+    tries += 1
+    try:
+      return api_call(*args,**kwargs)
+    except Exception, e:
+      last_exception = e
+      code = get_api_error_code(e)
+      if retry<=0:
+        raise RetryLimitExceeded(last_exception,tries)
+      elif retry>0 and (code == "Throttling" or code == "RequestLimitExceeded"):
+        retry -= 1
+        retry_interval += 1
+        time.sleep(retry_interval)
+      else:
+        raise e
+  return None
